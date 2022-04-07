b31038622e0d08e861db220f6d16959dca5efcfc
Hapro Bishop <acfowo45@gmail.com>
2022-04-07 14:06:59 +0300

    Исправлена проблема класса для Unit теста

--- UnitTest/UnitTestClass.cs
+++ UnitTest/UnitTestClass.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using SorterLab1;
+using SorterLib;
 
 namespace UnitTest
 {
@@ -8,13 +9,18 @@ namespace UnitTest
     public class UnitTestClass
     {
         [TestMethod]
-        public void TestAll()
+        public void TestSort()
         {
             int[] mustMas = { -7, -6, -3, 0, 6 };
             int[] mas = {-6, 6, 0, -7, -3 };
-            int[] sortedMas = SorterLib.Sorter.SortOnUp(mas);
+            int[] sortedMas = Sorter.SortOnUp(mas);
+            CollectionAssert.AreEqual(mustMas, sortedMas);
+        }
+        [TestMethod]
+        public void TestEven()
+        {
             string text = "Нечет";
-            Assert.Equal(mustMas, sortedMas);
+            Assert.AreEqual(text, MainWindow.CheckEven(-7, 6));
         }
     }
 }
